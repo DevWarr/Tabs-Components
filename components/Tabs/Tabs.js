f099949d0625ba0917c279bdd6065a466f08c3f5
Devin Warrick <dev.warr@gmail.com>
2019-05-16 18:06:14 -0500

    Carousel complete. No animation on carousel, but images do rotate.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,11 +6,9 @@ class Tabs {
     
     // Get the custom data attribute on the Link
     this.data = element.dataset.tab;
-    console.log(this.data);
     
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem = new TabItem(this.itemElement);
